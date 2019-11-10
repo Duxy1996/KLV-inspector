ad56d1fe0f5f079ba7c22364764f4e950c596ac2
duxy1996 <durancdr96@gmail.com>
2019-11-10 12:25:58 +0100

    JS - Added basic decript for the klm file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 
-var heyHeader = ["06", "0E", "2B", "34", "02", "0B", "01", "01", "0E", "01", "03", "01", "01", "00", "00", "00"];
+var heyHeader = [0x06, 0x0E, 0x2B, 0x34, 0x02, 0x0B, 0x01, 0x01, 0x0E, 0x01, 0x03, 0x01, 0x01, 0x00, 0x00, 0x00];
 
 var registredData = []
 
@@ -18,7 +18,7 @@ function printHexCharcters(e)
     }
 
     byteStr = byteStr.toUpperCase();
-    registredData.push(byteStr);
+    registredData.push(aByte);
     str_cus = str_cus + byteStr + ", ";
   }
   document.getElementById("demo").innerHTML = str_cus;
@@ -26,14 +26,23 @@ function printHexCharcters(e)
 
 function analyse()
 {
-  for (let step = 0; step < registredData.length; step)
+  let step = 0
+  for (let headerStep = 0; headerStep < heyHeader.length; headerStep++)
   {
-    for (let headerStep = 0; headerStep < heyHeader.length; headerStep++)
-    {
-      console.log(registredData[step], "----", heyHeader[headerStep]);
-      step++;
-    }
-    console.log(registredData[step++]);
+    console.log(registredData[step], "----", heyHeader[headerStep]);
+    step++;
+  }
+
+  let messageSize = registredData[step++] & parseInt("1111",2);
+  let totalSize = 0;
+  for (let packSize = messageSize; packSize > 0; packSize--)
+  {
+    totalSize += registredData[step++] << (packSize - 1) * 8;
+  }
+  console.log(totalSize);
+
+  for (let packSize = messageSize; packSize < totalSize; packSize++)
+  {
     console.log(registredData[step++]);
   }
 }
